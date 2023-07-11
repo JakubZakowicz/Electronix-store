603d4c3a2d0495faa0c579300c4100962a3b505f
JakubZakowicz <j.zakowicz99@gmail.com>
2023-07-11 19:45:07 +0200

    #3 - add navigation

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,95 +1,98 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Collapse,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import Logo from '../public/logo.svg';
+import styles from './page.module.css';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import GridViewIcon from '@mui/icons-material/GridView';
+import { ExpandLess, ExpandMore, StarBorder } from '@mui/icons-material';
 
 export default function Home() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box>
+      <Box
+        margin="30px"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Image src={Logo} width={60} alt="logo" />
+        <Box width="700px">
+          <TextField
+            hiddenLabel
+            className={styles.textfield}
+            variant="standard"
+            fullWidth
+            placeholder="Search"
+            InputProps={{
+              disableUnderline: true,
+              startAdornment: (
+                <SearchIcon sx={{ color: '#808080', marginRight: '5px' }} />
+              ),
+            }}
+          />
+        </Box>
+        <Box display="flex" alignItems="center" gap="40px">
+          <PersonIcon sx={{ fontSize: '40px' }} />
+          <ShoppingCartIcon fontSize="large" />
+        </Box>
+      </Box>
+      <Drawer
+        sx={{
+          width: 300,
+          flexShrink: 0,
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          '& .MuiDrawer-paper': {
+            width: 300,
+            boxSizing: 'border-box',
+            backgroundColor: 'transparent',
+            color: 'white',
+            marginTop: 15,
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <List>
+          <ListItemButton onClick={handleClick}>
+            <ListItemIcon>
+              <GridViewIcon sx={{ color: 'white' }} />
+            </ListItemIcon>
+            <ListItemText primary="Categories" />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="Virtual Reality" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+        </List>
+      </Drawer>
+    </Box>
+  );
 }
