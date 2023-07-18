dfc4eaa539e02cc1755244c2dde09a05cdecd395
Jakub Zakowicz <63516552+JakubZakowicz@users.noreply.github.com>
2023-07-18 19:23:25 +0200

    #4 - add products page (#21)
    * #9 - add products page
    
    * #4 - add unit tests

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -10,9 +10,8 @@ import {
   Grid,
   Typography,
 } from '@mui/material';
-import StarIcon from '@mui/icons-material/Star';
 import Vision from '../images/vision.png';
-import VRHeadset from '../images/HP_Reverb.png';
+import Products from '@/components/Products';
 
 export default function Home() {
   return (
@@ -57,57 +56,7 @@ export default function Home() {
           </Grid>
         </CardContent>
       </Card>
-      <Typography
-        marginTop="50px"
-        variant="h2"
-        fontSize="30px"
-        fontWeight="bold"
-      >
-        Top Products
-      </Typography>
-      <Grid container marginTop="-30px" spacing={2} rowSpacing={6}>
-        {[
-          'HP Reverb G2',
-          'HP Reverb G2',
-          'HP Reverb G2',
-          'HP Reverb G2',
-          'HP Reverb G2',
-        ].map((text) => (
-          <Grid key={text} item sm={12} lg={4} xl={3}>
-            <Card
-              sx={{
-                padding: '5px',
-                background: 'rgba(255, 255, 255, 0.1)',
-                borderRadius: '20px',
-                border: '1px solid white',
-                marginTop: '10px',
-                color: 'white',
-                width: '300px',
-              }}
-            >
-              <CardContent>
-                <Image
-                  src={VRHeadset}
-                  width="250"
-                  height="200"
-                  alt="VR headset"
-                />
-                <Typography fontSize="20px" marginTop="10px">
-                  {text}
-                </Typography>
-                <Box marginTop="10px" color="yellow">
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                </Box>
-                <Typography marginTop="10px">$39.68</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+     <Products name="Top Products" />
     </Box>
   );
 }
---
+++ client/components/Products/index.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import React from 'react';
+import Image from 'next/image';
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
+import VRHeadset from '../../images/HP_Reverb.png';
+
+interface ProductsInterface {
+  name: string
+}
+
+const Products = ({ name }: ProductsInterface) => {
+  return (
+    <Box>
+      <Typography
+        marginTop="50px"
+        variant="h2"
+        fontSize="30px"
+        fontWeight="bold"
+      >
+        {name}
+      </Typography>
+      <Grid container marginTop="-30px" spacing={12} rowSpacing={6}>
+        {[
+          'HP Reverb G2',
+          'HP Reverb G2',
+          'HP Reverb G2',
+          'HP Reverb G2',
+          'HP Reverb G2',
+        ].map((text) => (
+          <Grid key={text} item sm={12} lg={4} xl={3}>
+            <Card
+              sx={{
+                padding: '5px',
+                background: 'rgba(255, 255, 255, 0.1)',
+                borderRadius: '20px',
+                border: '1px solid white',
+                marginTop: '10px',
+                color: 'white',
+                width: '300px',
+              }}
+            >
+              <CardContent>
+                <Image
+                  src={VRHeadset}
+                  width="250"
+                  height="200"
+                  alt="VR headset"
+                />
+                <Typography fontSize="20px" marginTop="10px">
+                  {text}
+                </Typography>
+                <Box marginTop="10px" color="yellow">
+                  <StarIcon />
+                  <StarIcon />
+                  <StarIcon />
+                  <StarIcon />
+                  <StarIcon />
+                </Box>
+                <Typography marginTop="10px">$39.68</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default Products;
