caeb92c215ee435aa00ce93355eb4aca1d851b46
JakubZakowicz <j.zakowicz99@gmail.com>
2023-08-07 18:32:36 +0200

    #30 - finish purchase history

--- client/src/app/categories/[category]/page.tsx
+++ client/src/app/categories/[category]/page.tsx
@@ -1,5 +1,5 @@
-import Products from '../../../components/Products';
 import React from 'react';
+import Products from '../../../components/Products';
 
 const ProductsPage = () => {
   return (
--- client/src/app/order-history/page.tsx
+++ client/src/app/order-history/page.tsx
@@ -10,7 +10,9 @@ import {
   Grid,
   Typography,
 } from '@mui/material';
+import CircleIcon from '@mui/icons-material/Circle';
 import VR1 from '@/src/images/vr1.png';
+import VR2 from '@/src/images/HP_Reverb.png';
 
 const OrderHistory = () => {
   return (
@@ -31,25 +33,50 @@ const OrderHistory = () => {
         }}
       >
         <CardContent>
-          <Typography>Package Status: Delivered</Typography>
-          <Divider sx={{ background: 'white', margin: '25px 0' }} />
-          <Grid container spacing={3}>
-            <Grid item xs={1} sm={2} xl={1}>
-              <Image src={VR1} width="100" height="100" alt="image" />
-            </Grid>
-            <Grid item xs={12} sm={3} lg={5}>
-              <Typography>
-                Meta Quest 2 — Advanced All-In-One Virtual Reality Headset — 128
-                GB
-              </Typography>
-            </Grid>
-            <Grid item xs={2} sm={3} lg={3}>
-              <Typography textAlign="right">1 x $299.00</Typography>
-            </Grid>
-            <Grid item xs={2} sm={2} lg={3}>
-              <Typography textAlign="right">$299.00</Typography>
-            </Grid>
-          </Grid>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+            <Typography>Package Status:</Typography>
+            <CircleIcon sx={{ color: '#2DFF0B', fontSize: '18px', marginLeft: '5px' }} />
+            <Typography> Delivered</Typography>
+          </Box>
+
+          <Divider sx={{ background: 'white', marginTop: '25px' }} />
+          {orderHistoryItems.map(({ image, name, price, amount, total }) => (
+            <Box key={name}>
+              <Grid container spacing={3} sx={{ padding: '25px 0' }}>
+                <Grid item xs={1} sm={2} xl={1}>
+                  <Image src={image} width="100" height="100" alt="image" />
+                </Grid>
+                <Grid item xs={12} sm={3} lg={5}>
+                  <Typography>{name}</Typography>
+                </Grid>
+                <Grid item xs={2} sm={3} lg={3}>
+                  <Typography textAlign="right">
+                    {amount} x ${price}
+                  </Typography>
+                </Grid>
+                <Grid item xs={2} sm={2} lg={3}>
+                  <Typography textAlign="right">${total}</Typography>
+                </Grid>
+              </Grid>
+              <Divider sx={{ background: 'white' }} />
+            </Box>
+          ))}
+          <Box
+            sx={{
+              float: 'right',
+              margin: '40px 0',
+              '& p': { fontSize: '20px' },
+            }}
+          >
+            <Box sx={{ display: 'flex', gap: '80px' }}>
+              <Typography>Delivery:</Typography>
+              <Typography>$10</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', gap: '80px', marginTop: '10px' }}>
+              <Typography>Total:</Typography>
+              <Typography>$348.99</Typography>
+            </Box>
+          </Box>
         </CardContent>
       </Card>
     </Box>
@@ -57,3 +84,20 @@ const OrderHistory = () => {
 };
 
 export default OrderHistory;
+
+const orderHistoryItems = [
+  {
+    image: VR1,
+    name: 'Meta Quest 2 — Advanced All-In-One Virtual Reality Headset — 128GB',
+    price: '299.00',
+    amount: 1,
+    total: '299.00',
+  },
+  {
+    image: VR2,
+    name: 'HP Reverb G2',
+    price: '39.99',
+    amount: 1,
+    total: '39.99',
+  },
+];
