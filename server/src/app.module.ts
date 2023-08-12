1f2aec0cba1294eb1004f997956fcfb92505994b
JakubZakowicz <j.zakowicz99@gmail.com>
2023-08-12 16:00:11 +0200

    #48 - add category GET endpoint

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { Category } from './typeorm/entities/Category';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'admin',
+      password: 'password',
+      database: 'ecommerce',
+      entities: [Category],
+      synchronize: true,
+    }),
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/categories/controllers/categories/categories.controller.ts
+++ server/src/categories/controllers/categories/categories.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { CategoriesService } from '../../services/categories/categories.service';
 
 @Controller('categories')
 export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
   @Get('/')
   getCategories() {
-    return [];
+    return this.categoriesService.findCategories();
   }
 }
