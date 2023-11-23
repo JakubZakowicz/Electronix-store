37c2f0453eef55d7defb6685999659df46c50854
JakubZakowicz <j.zakowicz99@gmail.com>
2023-11-23 17:20:19 +0100

    #113 - fix pagination decorator

--- server/src/decorators/pagination-params.decorator.ts
+++ server/src/decorators/pagination-params.decorator.ts
@@ -15,15 +15,16 @@ export interface Pagination {
 export const PaginationParams = createParamDecorator(
   (data, ctx: ExecutionContext): Pagination => {
     const req: Request = ctx.switchToHttp().getRequest();
-    const page = parseInt(req.query.page as string);
-    const size = parseInt(req.query.size as string);
+    const maxSize = 100;
+    const page = parseInt(req.query.page as string) || 0;
+    const size = parseInt(req.query.size as string) || maxSize;
 
     // check if page and size are valid
     if (isNaN(page) || page < 0 || isNaN(size) || size < 0) {
       throw new BadRequestException('Invalid pagination params');
     }
     // do not allow to fetch large slices of the dataset
-    if (size > 100) {
+    if (size > maxSize) {
       throw new BadRequestException(
         'Invalid pagination params: Max size is 100',
       );
