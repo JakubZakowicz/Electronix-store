9b300100c48a20f4f5b028e1c6d11318a8a348b3
JakubZakowicz <j.zakowicz99@gmail.com>
2024-02-27 20:29:28 +0100

    #119 - add sorting functionality

--- server/src/utils/functions.ts
+++ server/src/utils/functions.ts
@@ -1,3 +1,8 @@
+import { Sorting } from '../decorators/sorting-params.decorator';
+
 export const getPageCount = (itemsCount: number, limit: number): number => {
   return Math.ceil(itemsCount / limit);
 };
+
+export const getOrder = (sort?: Sorting) =>
+  sort ? { [sort.property]: sort.direction } : {};
