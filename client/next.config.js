f4b040a26e34da56fb1accfc481431d364e3e6ff
JakubZakowicz <j.zakowicz99@gmail.com>
2024-04-30 20:00:08 +0200

    #123 - add images to seeder-data.json

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['images.pexels.com', 'images.unsplash.com'] },
+  images: {
+    domains: ['images.pexels.com', 'images.unsplash.com', 'm.media-amazon.com'],
+  },
 };
 
 module.exports = nextConfig;
