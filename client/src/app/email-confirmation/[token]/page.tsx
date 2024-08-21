80022ac128a63422c4a3a3a09b6ca60ec1aae207
JakubZakowicz <j.zakowicz99@gmail.com>
2024-08-21 22:08:07 +0200

    #80 - refactor code

--- client/src/app/email-confirmation/[token]/page.tsx
+++ client/src/app/email-confirmation/[token]/page.tsx
@@ -1,11 +1,6 @@
 'use client';
 
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-import CancelIcon from '@mui/icons-material/Cancel';
-import DefaultButton from '@/src/components/DefaultButton';
-import Link from 'next/link';
-import { pageRoutes } from '@/src/routes/pageRoutes';
 import { useConfirmEmail } from '@/src/api/auth';
 import ResultMessage from '@/src/components/ResultMessage';
 
@@ -28,29 +23,13 @@ const EmailConfirmation = ({ params }: EmailConfirmationProps) => {
         <ResultMessage message={emailConfirmationResult?.message} />
       )}
       {isEmailConfirmationError && (
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            color: '#D0312D',
-          }}
-        >
-          <CancelIcon sx={{ fontSize: 200, marginTop: 5 }} />
-          <Typography variant="h2" sx={{ fontSize: 40, marginTop: 3 }}>
-            {
-              (emailConfirmationError?.response?.data as { message: string })
-                ?.message
-            }
-          </Typography>
-          <Link href={pageRoutes.root()}>
-            <DefaultButton
-              style={{ marginTop: '40px' }}
-              name="Go Back to main page"
-            />
-          </Link>
-        </Box>
+        <ResultMessage
+          message={
+            (emailConfirmationError?.response?.data as { message: string })
+              ?.message
+          }
+          isSuccess={false}
+        />
       )}
     </>
   );
--- client/src/components/ResultMessage/index.tsx
+++ client/src/components/ResultMessage/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Link from 'next/link';
 import { Box, Typography } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 import { pageRoutes } from '@/src/routes/pageRoutes';
 import DefaultButton from '../DefaultButton';
 
 interface ResultMessageProps {
   message: string;
+  isSuccess?: boolean;
 }
 
-const ResultMessage = ({ message }: ResultMessageProps) => {
-  return (
+const ResultMessage = ({ message, isSuccess = true }: ResultMessageProps) => {
+  return isSuccess ? (
     <Box
       sx={{
         display: 'flex',
@@ -21,6 +23,27 @@ const ResultMessage = ({ message }: ResultMessageProps) => {
       }}
     >
       <CheckCircleIcon sx={{ fontSize: 200, marginTop: 5 }} />
+      <Typography variant="h2" sx={{ fontSize: 40, marginTop: 3 }}>
+        {message}
+      </Typography>
+      <Link href={pageRoutes.root()}>
+        <DefaultButton
+          style={{ marginTop: '40px' }}
+          name="Go Back to main page"
+        />
+      </Link>
+    </Box>
+  ) : (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        color: '#D0312D',
+      }}
+    >
+      <CancelIcon sx={{ fontSize: 200, marginTop: 5 }} />
       <Typography variant="h2" sx={{ fontSize: 40, marginTop: 3 }}>
         {message}
       </Typography>
